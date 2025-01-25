313daf9ab545180c3aeefd01f5e34d2d2731ee54
Klemen Slavič <krof.drakula@gmail.com>
2025-01-25 16:52:41 +0100

    Cleanup

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { resolve } from "node:path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    lib: {
+      entry: resolve(import.meta.dirname, "src/main.ts"),
+      name: "UIKit",
+      fileName: "ui-kit",
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+    },
+  },
+});
